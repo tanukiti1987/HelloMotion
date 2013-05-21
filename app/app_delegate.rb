8e11c901ec0117ba96ba9f9d5b830fec18f06bae
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2013-05-21 21:43:46 +0900

    Fix using application frame size

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,6 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = SampleViewController.alloc.init
     @window.rootViewController.wantsFullScreenLayout = true
     @window.makeKeyAndVisible
